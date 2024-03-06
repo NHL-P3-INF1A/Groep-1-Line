4eff1480726083ffaba5381826a58ed70ce86230
Bram Suurd <bram.suurd@student.nhlstenden.com>
2024-03-06 11:51:49 +0100

    Update turn adjustments and add black square detection

--- Groep-1-Line.ino
+++ Groep-1-Line.ino
@@ -12,46 +12,23 @@ const int linePins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 const int lineThreshold = 700;
 
 int heavyTurnAdjustment = 255;
-int hardTurnAdjustment = 175;
-int strongTurnAdjustment = 125;
+int hardTurnAdjustment = 200;
+
+int strongTurnAdjustment = 150;
 int veerAdjustment = 25;
 
 // Base speed of the robot
 const int baseSpeedLeft = 255;
 const int baseSpeedRight = 255;
-const int startSpeed = 200; 
 
-/*///////////////// functions /////////////////*/
+int delayTime = 1000;
 
-void checkIfOnBlackSquare() {
-  // Define the missing variables
-  bool evenlessFarLeftOnLine;
-  bool evenlessFarRightOnLine;
-  bool lessFarLeftOnLine;
-  bool lessFarRightOnLine;
-  bool farLeftOnLine;
-  bool farRightOnLine;
-  bool extremeLeft;
-  bool extremeRight;
-  int baseSpeed = 255;
-  int delayTime = 1000;
-
-  if ((evenlessFarLeftOnLine + evenlessFarRightOnLine + lessFarLeftOnLine + lessFarRightOnLine + farLeftOnLine + farRightOnLine + extremeLeft + extremeRight) >= 3)
-  {
-    analogWrite(motorA1, baseSpeed);
-    analogWrite(motorB1, baseSpeed);
-    delay(delayTime); // Add missing parentheses and semicolon
-    if ((evenlessFarLeftOnLine + evenlessFarRightOnLine + lessFarLeftOnLine + lessFarRightOnLine + farLeftOnLine + farRightOnLine + extremeLeft + extremeRight) >= 3)
-    {
-      analogWrite(motorA1, 0);
-      analogWrite(motorB1, 0);
-    }
-  } 
-}
+bool endOfLine = false;
 
 /*///////////////// setup /////////////////*/
 
-void setup() {
+void setup()
+{
   // Initialize motor control pins as outputs
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
@@ -59,83 +36,104 @@ void setup() {
   pinMode(motorB2, OUTPUT);
 
   // Initialize line sensor pins as inputs
-  pinMode (A0, INPUT);
-  pinMode (A1, INPUT);
-  pinMode (A2, INPUT);
-  pinMode (A3, INPUT);
-  pinMode (A4, INPUT);
-  pinMode (A5, INPUT);
-  pinMode (A6, INPUT);
-  pinMode (A7, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
 }
+/*///////////////// Function ///////////////*/
 
+void blacksquare()
+{
+  if ((evenlessFarLeftOnLine + evenlessFarRightOnLine + lessFarLeftOnLine + lessFarRightOnLine + farLeftOnLine + farRightOnLine + extremeLeft + extremeRight) >= 3)
+  {
+    analogWrite(motorA1, baseSpeedLeft);
+    analogWrite(motorB1, baseSpeedRight);
+    delay(delayTime);
+    if ((evenlessFarLeftOnLine + evenlessFarRightOnLine + lessFarLeftOnLine + lessFarRightOnLine + farLeftOnLine + farRightOnLine + extremeLeft + extremeRight) >= 3)
+    {
+      endOfLine = true;
+    }
+  }
+}
 
 /*///////////////// follow line loop /////////////////*/
 
-void loop() {
-  // Read sensor values
-  int sensorValues[8];
-  for (int i = 0; i < 8; i++) {
-    sensorValues[i] = analogRead(linePins[i]);
+void loop()
+
+// Read sensor values
+int sensorValues[8];
+for (int i = 0; i < 8; i++)
+{
+  sensorValues[i] = analogRead(linePins[i]);
+}
+
+// Determine if sensors are on the line
+bool extremeLeft = sensorValues[0] > lineThreshold;
+bool farLeftOnLine = sensorValues[1] > lineThreshold;
+bool lessFarLeftOnLine = sensorValues[2] > lineThreshold;
+bool evenlessFarLeftOnLine = sensorValues[3] > lineThreshold;
+bool extremeRight = sensorValues[7] > lineThreshold;
+bool farRightOnLine = sensorValues[6] > lineThreshold;
+bool lessFarRightOnLine = sensorValues[5] > lineThreshold;
+bool evenlessFarRightOnLine = sensorValues[4] > lineThreshold;
+{
+
+  blacksquare();
+  if (endOfLine)
+  {
+    analogWrite(motorA1, 0);
+    analogWrite(motorB1, 0);
   }
+  
 
-  // Determine if sensors are on the line
-  bool extremeLeft = sensorValues[0] > lineThreshold;
-  bool farLeftOnLine = sensorValues[1] > lineThreshold;
-  bool lessFarLeftOnLine = sensorValues[2] > lineThreshold;
-  bool evenlessFarLeftOnLine = sensorValues[3] > lineThreshold;
-  bool extremeRight = sensorValues[7] > lineThreshold;
-  bool farRightOnLine = sensorValues[6] > lineThreshold;
-  bool lessFarRightOnLine = sensorValues[5] > lineThreshold;
-  bool evenlessFarRightOnLine = sensorValues[4] > lineThreshold;
-
-  // Check if sensors 3 and 4 are active
-  bool sensors3And4Active = sensorValues[3] > lineThreshold && sensorValues[4] > lineThreshold;
-
-  // NEW SECTION: Check for cross-section
-  bool allCenterSensorsActive = lessFarLeftOnLine && evenlessFarLeftOnLine && evenlessFarRightOnLine && lessFarRightOnLine;
-
-  if (allCenterSensorsActive && !sensors3And4Active) {
-    // Cross-section detected - go straight
-    analogWrite(motorA1, baseSpeedLeft);
-    analogWrite(motorB1, baseSpeedRight);
-  } else if (farLeftOnLine) {
+  // Determine motor speeds
+  if (farLeftOnLine)
+  {
     // Hard right turn
-    analogWrite(motorA1, baseSpeedLeft - hardTurnAdjustment); // Even slower left motor
-    analogWrite(motorB1, baseSpeedRight - 50);
-  } else if (lessFarLeftOnLine) {
+    analogWrite(motorA1, baseSpeedLeft - hardTurnAdjustment);
+    analogWrite(motorB1, baseSpeedRight);
+  }
+  else if (lessFarLeftOnLine)
+  {
     // Strong right turn
     analogWrite(motorA1, baseSpeedLeft - strongTurnAdjustment);
-    analogWrite(motorB1, baseSpeedRight - 35);
-  } else if (evenlessFarLeftOnLine) {
+    analogWrite(motorB1, baseSpeedRight);
+  }
+  else if (evenlessFarLeftOnLine)
+  {
     // Veer left
-    analogWrite(motorA1, baseSpeedLeft);
-    analogWrite(motorB1, baseSpeedRight - veerAdjustment);
-  } else if (evenlessFarRightOnLine) {
-    // Veer right
     analogWrite(motorA1, baseSpeedLeft - veerAdjustment);
     analogWrite(motorB1, baseSpeedRight);
-  } else if (lessFarRightOnLine) {
-    // Strong left turn
-    analogWrite(motorA1, baseSpeedLeft - 35);
-    analogWrite(motorB1, baseSpeedRight - strongTurnAdjustment * 2);
-  } else if (farRightOnLine) {
-    // Hard left turn
-    analogWrite(motorA1, baseSpeedLeft - 50);
-    analogWrite(motorB1, baseSpeedRight - hardTurnAdjustment * 2);
   }
-
-  if (extremeLeft && !sensors3And4Active) {
-    // Lost the line to the extreme left - rotate heavy
-    analogWrite(motorA1, baseSpeedLeft - heavyTurnAdjustment);
-    analogWrite(motorB1, baseSpeedRight);
-  } else if (extremeRight && !sensors3And4Active) {
-    // Lost the line to the extreme right - rotate heavy
+  else if (evenlessFarRightOnLine)
+  {
+    // Veer right
     analogWrite(motorA1, baseSpeedLeft);
-    analogWrite(motorB1, baseSpeedRight - heavyTurnAdjustment);
+    analogWrite(motorB1, baseSpeedRight - veerAdjustment);
   }
-
-  checkIfOnBlackSquare();
+  else if (lessFarRightOnLine)
+  {
+    // Strong left turn
+    analogWrite(motorA1, baseSpeedLeft);
+    analogWrite(motorB1, baseSpeedRight - strongTurnAdjustment);
+  }
+  else if (farRightOnLine)
+  {
+    // Hard left turn
+    analogWrite(motorA1, baseSpeedLeft);
+    analogWrite(motorB1, baseSpeedRight - hardTurnAdjustment);
+  }
+  else
+  {
+    // Drive straight
+    analogWrite(motorA1, baseSpeedLeft);
+    analogWrite(motorB1, baseSpeedRight);
+  }
 
   digitalWrite(motorA2, LOW);
   digitalWrite(motorB2, LOW);
