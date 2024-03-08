f59cdb76ff08427a791ee45e80a1a7210dc429f9
Bram Suurd <bram.suurd@student.nhlstenden.com>
2024-03-08 16:33:31 +0100

    Add Arduino configuration and gripper control

--- Groep-1-Line.ino
+++ Groep-1-Line.ino
@@ -11,6 +11,8 @@ const int linePins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
 const int lineThreshold = 700;
 
+const int gripperPin = 5;
+
 int heavyTurnAdjustment = 255;
 int hardTurnAdjustment = 200;
 
@@ -18,17 +20,80 @@ int strongTurnAdjustment = 150;
 int veerAdjustment = 25;
 
 // Base speed of the robot
-const int baseSpeedLeft = 255;
+const int baseSpeedLeft = 240;
 const int baseSpeedRight = 255;
+const int startSpeed = 200;
 
 int delayTime = 1000;
 
 bool endOfLine = false;
 
+const int SENSOR_COUNT = 8;
+int maxValues[SENSOR_COUNT] = {0};
+int minValues[SENSOR_COUNT] = {1023};
+int calibrationThreshold[SENSOR_COUNT];
+
+bool endOfBlackSquare = analogRead(A4) > lineThreshold;
+
+/*///////////////// Function ///////////////*/
+
+void calibrateSensors()
+{
+  analogWrite(motorA1, startSpeed);
+  analogWrite(motorB1, startSpeed);
+  digitalWrite(motorA2, LOW);
+  digitalWrite(motorB2, LOW);
+  delay(1500);
+
+  for (int i = 0; i < SENSOR_COUNT; i++)
+  {
+    int sensorValue = analogRead(linePins[i]);
+    maxValues[i] = max(maxValues[i], sensorValue);
+    minValues[i] = min(minValues[i], sensorValue);
+
+    calibrationThreshold[i] = (maxValues[i] + minValues[i]) / 2 + 100;
+  }
+
+  if (endOfBlackSquare)
+  {
+    analogWrite(motorA1, 0);
+    analogWrite(motorB1, 0);
+    gripperClose();
+    delay(500);
+    analogWrite(motorA2, 250);
+    analogWrite(motorB1, 250);
+    delay(500);
+    analogWrite(motorA2, 0);
+    analogWrite(motorB2, 0);
+  }
+}
+
+void gripperOpen()
+{
+  moveGripper(1700);
+}
+
+void gripperClose()
+{
+  moveGripper(1050);
+}
+
+void moveGripper(int pulseDuration)
+{
+  for (int i = 0; i < 10; i++)
+  {
+    delay(20);
+    digitalWrite(gripperPin, HIGH);
+    delayMicroseconds(pulseDuration);
+    digitalWrite(gripperPin, LOW);
+  }
+}
+
 /*///////////////// setup /////////////////*/
 
 void setup()
 {
+  gripperOpen();
   // Initialize motor control pins as outputs
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
@@ -44,97 +109,75 @@ void setup()
   pinMode(A5, INPUT);
   pinMode(A6, INPUT);
   pinMode(A7, INPUT);
-}
-/*///////////////// Function ///////////////*/
 
-void blacksquare()
-{
-  if ((evenlessFarLeftOnLine + evenlessFarRightOnLine + lessFarLeftOnLine + lessFarRightOnLine + farLeftOnLine + farRightOnLine + extremeLeft + extremeRight) >= 3)
-  {
-    analogWrite(motorA1, baseSpeedLeft);
-    analogWrite(motorB1, baseSpeedRight);
-    delay(delayTime);
-    if ((evenlessFarLeftOnLine + evenlessFarRightOnLine + lessFarLeftOnLine + lessFarRightOnLine + farLeftOnLine + farRightOnLine + extremeLeft + extremeRight) >= 3)
-    {
-      endOfLine = true;
-    }
-  }
+  calibrateSensors();
 }
 
 /*///////////////// follow line loop /////////////////*/
 
 void loop()
-
-// Read sensor values
-int sensorValues[8];
-for (int i = 0; i < 8; i++)
 {
-  sensorValues[i] = analogRead(linePins[i]);
-}
+  bool extremeLeft = analogRead(A0) > lineThreshold;
+  bool farLeftOnLine = analogRead(A1) > lineThreshold;
+  bool lessFarLeftOnLine = analogRead(A2) > lineThreshold;
+  bool evenlessFarLeftOnLine = analogRead(A3) > lineThreshold;
+  bool extremeRight = analogRead(A7) > lineThreshold;
+  bool farRightOnLine = analogRead(A6) > lineThreshold;
+  bool lessFarRightOnLine = analogRead(A5) > lineThreshold;
+  bool evenlessFarRightOnLine = analogRead(A4) > lineThreshold;
 
-// Determine if sensors are on the line
-  bool extremeLeft = sensorValues[0] > lineThreshold;
-  bool farLeftOnLine = sensorValues[1] > lineThreshold;
-  bool lessFarLeftOnLine = sensorValues[2] > lineThreshold;
-  bool evenlessFarLeftOnLine = sensorValues[3] > lineThreshold;
-  bool extremeRight = sensorValues[7] > lineThreshold;
-  bool farRightOnLine = sensorValues[6] > lineThreshold;
-  bool lessFarRightOnLine = sensorValues[5] > lineThreshold;
-  bool evenlessFarRightOnLine = sensorValues[4] > lineThreshold;
-{
-
-  blacksquare();
-  if (endOfLine)
-  {
-    analogWrite(motorA1, 0);
-    analogWrite(motorB1, 0);
-  }
-  
-
-  // Determine motor speeds
   if (farLeftOnLine)
   {
     // Hard right turn
-    analogWrite(motorA1, baseSpeedLeft - hardTurnAdjustment);
-    analogWrite(motorB1, baseSpeedRight);
+    analogWrite(motorA1, baseSpeedLeft - hardTurnAdjustment); // Even slower left motor
+    analogWrite(motorB1, baseSpeedRight - 50);
   }
   else if (lessFarLeftOnLine)
   {
     // Strong right turn
     analogWrite(motorA1, baseSpeedLeft - strongTurnAdjustment);
-    analogWrite(motorB1, baseSpeedRight);
+    analogWrite(motorB1, baseSpeedRight - 35);
   }
   else if (evenlessFarLeftOnLine)
   {
     // Veer left
-    analogWrite(motorA1, baseSpeedLeft - veerAdjustment);
-    analogWrite(motorB1, baseSpeedRight);
+    analogWrite(motorA1, baseSpeedLeft);
+    analogWrite(motorB1, baseSpeedRight - veerAdjustment);
   }
   else if (evenlessFarRightOnLine)
   {
     // Veer right
-    analogWrite(motorA1, baseSpeedLeft);
-    analogWrite(motorB1, baseSpeedRight - veerAdjustment);
+    analogWrite(motorA1, baseSpeedLeft - veerAdjustment);
+    analogWrite(motorB1, baseSpeedRight);
   }
   else if (lessFarRightOnLine)
   {
     // Strong left turn
-    analogWrite(motorA1, baseSpeedLeft);
-    analogWrite(motorB1, baseSpeedRight - strongTurnAdjustment);
+    analogWrite(motorA1, baseSpeedLeft - 35);
+    analogWrite(motorB1, baseSpeedRight);
   }
   else if (farRightOnLine)
   {
     // Hard left turn
-    analogWrite(motorA1, baseSpeedLeft);
-    analogWrite(motorB1, baseSpeedRight - hardTurnAdjustment);
+    analogWrite(motorA1, baseSpeedLeft - 50);
+    analogWrite(motorB1, baseSpeedRight);
+  }
+  else if (extremeLeft)
+  {
+    // Hard right turn
+    analogWrite(motorA1, 150);
+    analogWrite(motorB1, baseSpeedRight - 50);
+  }
+  else if (extremeRight)
+  {
+    // Hard left turn
+    analogWrite(motorA1, baseSpeedLeft - 50);
+    analogWrite(motorB1, 150);
   }
   else
   {
-    // Drive straight
+    // Move forward
     analogWrite(motorA1, baseSpeedLeft);
     analogWrite(motorB1, baseSpeedRight);
   }
-
-  digitalWrite(motorA2, LOW);
-  digitalWrite(motorB2, LOW);
 }
