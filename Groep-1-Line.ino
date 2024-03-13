2edad4d4225bae8cebc7e796eec4fcc4a3e6e0a0
elmedinarifi <49024630+elmedinarifi@users.noreply.github.com>
2024-03-13 11:32:42 +0100

    LedLampjes Toegevoegd

--- Groep-1-Line.ino
+++ Groep-1-Line.ino
@@ -1,66 +1,58 @@
-// ==== [ Motor Pins ] ========================================================
-#define motorA1                 6   // Motor A1 LF
-#define motorA2                 9   // Motor A2 LB
-#define motorB1                 10  // Motor B1 RF
-#define motorB2                 11  // Motor B2 RB
+/*///////////////// define variables /////////////////*/
+const int motorA1 = 6;
+const int motorA2 = 9;
+const int motorB1 = 10;
+const int motorB2 = 11;
+const int linePins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
-// ==== [ Line Sensor Pins ] ===================================================
-const int linePins[] = {A0, A1, A2, A3, A4, A5, A6, A7}; // 8 Line sensors
+#include <Adafruit_NeoPixel.h>
+#define   LED_PIN                 13   // Pin that the neopixels are connected to
+#define   LED_COUNT               4   // Amount of LEDs
+#define   BRIGHTNESS 50  // NeoPixel brightness (0-255)
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
 
-// ==== [ Gripper Pin ] ========================================================
-#define gripperPin              5 // Gripper servo
+#define   LED_LEFT_BACK           0   // Left Back
+#define   LED_RIGHT_BACK          1   // Right Back
+#define   LED_RIGHT_FRONT         2   // Right Front
+#define   LED_LEFT_FRONT          3   // Left Front
 
-// ==== [ Line Sensor Threshold ] ==============================================
-#define lineThreshold           700 // 0-1023
-#define heavyTurnAdjustment     255
-#define hardTurnAdjustment      175
-#define strongTurnAdjustment    125
-#define veerAdjustment          25
+const int gripperPin = 5;
 
-// ==== [ Motor Speeds ] =======================================================
-#define baseSpeed               255 // The base speed of the motors
-#define startSpeed              210 // The speed of the motors when starting
+const int lineThreshold = 700;
+int heavyTurnAdjustment = 255;
+int hardTurnAdjustment = 175;
+int strongTurnAdjustment = 125;
+int veerAdjustment = 25;
 
-// ==== [ Setup ] ==============================================================
+//Lampjes
+int lampjes = 13;
+
+const int ORANGE[] = {255, 80, 0};
+const int GREEN[] = {0, 255, 0};
+
+// Base speed of the robot
+const int baseSpeed = 255;
+const int startSpeed = 210; 
+
+/*///////////////// setup /////////////////*/
+
 void setup() {
-  setupLineSensors();
-  setupMotorPins();
+  // Initialize motor control pins as outputs
   gripperOpen();
-  initializeMotors();
-}
-
-void setupLineSensors() {
-  for (int i = 0; i < 8; i++) {
-    pinMode(linePins[i], INPUT);
-  }
-}
-
-void setupMotorPins() {
+  turnOnLED(0, 255, 80, 0); // Green
+  turnOnLED(1, 0, 255, 0); // Green
+  turnOnLED(2, 0, 255, 0); // Green
+  turnOnLED(3, 255, 80, 0); // Green
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
   pinMode(motorB2, OUTPUT);
   pinMode(gripperPin, OUTPUT);
-}
 
-void gripperOpen() {
-  moveGripper(1700);
-}
+  strip.begin();
+  strip.setBrightness(BRIGHTNESS);
+  strip.show();
 
-void gripperClose() {
-  moveGripper(1050);
-}
-
-void moveGripper(int pulseDuration) {
-  for (int i = 0; i < 10; i++) {
-    delay(20);
-    digitalWrite(gripperPin, HIGH);
-    delayMicroseconds(pulseDuration);
-    digitalWrite(gripperPin, LOW);
-  }
-}
-
-void initializeMotors() {
   analogWrite(motorA1, startSpeed);
   analogWrite(motorB1, startSpeed);
   digitalWrite(motorA2, LOW);
@@ -78,20 +70,37 @@ void initializeMotors() {
   delay(1000);
 }
 
-// ==== [ Loop ] ===============================================================
-void loop() {
-  int sensorValues[8];
-  readLineSensors(sensorValues);
-  determineLineFollowing(sensorValues);
+void turnOnLED(int led, uint8_t red, uint8_t green, uint8_t blue) {
+  // Set the color of the specified LED
+  strip.setPixelColor(led, red, green, blue);
+  strip.show();
 }
 
-void readLineSensors(int sensorValues[]) {
+void gripperOpen()
+{moveGripper(1700);}
+
+void gripperClose()
+{moveGripper(1050);}
+
+void moveGripper(int pulseDuration)
+{
+  for(int i = 0; i < 10; i++)
+  {
+    delay(20);
+    digitalWrite(gripperPin, HIGH);
+    delayMicroseconds(pulseDuration);
+    digitalWrite(gripperPin, LOW);
+  }
+}
+/*///////////////// follow line loop /////////////////*/
+void loop() {
+  //Read sensor values
+  int sensorValues[8];
   for (int i = 0; i < 8; i++) {
     sensorValues[i] = analogRead(linePins[i]);
   }
-}
-
-void determineLineFollowing(int sensorValues[]) {
+  
+  // Determine if sensors are on the line
   bool extremeLeft = sensorValues[0] > lineThreshold;
   bool farLeftOnLine = sensorValues[1] > lineThreshold;
   bool lessFarLeftOnLine = sensorValues[2] > lineThreshold;
@@ -100,87 +109,60 @@ void determineLineFollowing(int sensorValues[]) {
   bool farRightOnLine = sensorValues[6] > lineThreshold;
   bool lessFarRightOnLine = sensorValues[5] > lineThreshold;
   bool evenlessFarRightOnLine = sensorValues[4] > lineThreshold;
+  // Check if sensors 3 and 4 are active
   bool sensors3And4Active = sensorValues[3] > lineThreshold && sensorValues[4] > lineThreshold;
+  // NEW SECTION: Check for cross-section
   bool allCenterSensorsActive = lessFarLeftOnLine && evenlessFarLeftOnLine && evenlessFarRightOnLine && lessFarRightOnLine;
 
   if (allCenterSensorsActive && !sensors3And4Active) {
-    goStraight();
+    // Cross-section detected - go straight
+    analogWrite(motorA1, baseSpeed);
+    analogWrite(motorB1, baseSpeed);
+    turnOnLED(0, 255, 0, 0); 
   } else if (farLeftOnLine) {
-    hardRightTurn();
+    // Hard right turn
+    analogWrite(motorA1, baseSpeed - hardTurnAdjustment); // Even slower left motor
+    analogWrite(motorB1, baseSpeed - 50);
   } else if (lessFarLeftOnLine) {
-    strongRightTurn();
+    // Strong right turn
+    analogWrite(motorA1, baseSpeed - strongTurnAdjustment);
+    analogWrite(motorB1, baseSpeed - 35);
   } else if (evenlessFarLeftOnLine) {
-    veerLeft();
+    // Veer left
+    analogWrite(motorA1, baseSpeed);
+    analogWrite(motorB1, baseSpeed - veerAdjustment);
   } else if (evenlessFarRightOnLine) {
-    veerRight();
+    // Veer right
+    analogWrite(motorA1, baseSpeed - veerAdjustment);
+    analogWrite(motorB1, baseSpeed);
   } else if (lessFarRightOnLine) {
-    strongLeftTurn();
+    // Strong left turn
+    analogWrite(motorA1, baseSpeed - 35);
+    analogWrite(motorB1, baseSpeed - strongTurnAdjustment * 2);
   } else if (farRightOnLine) {
-    hardLeftTurn();
+    // Hard left turn
+    analogWrite(motorA1, baseSpeed - 50);
+    analogWrite(motorB1, baseSpeed - hardTurnAdjustment * 2);
   }
-
-  if (extremeLeft && !sensors3And4Active) {
-    rotateHeavyLeft();
+  else if (extremeLeft && !sensors3And4Active) {
+    // Lost the line to the extreme left - rotate heavy
+    analogWrite(motorA1, baseSpeed - heavyTurnAdjustment);
+    analogWrite(motorB1, baseSpeed);
   } else if (extremeRight && !sensors3And4Active) {
-    rotateHeavyRight();
-  } else if (allCenterSensorsActive) {
-    moveForward();
+    // Lost the line to the extreme right - rotate heavy
+    analogWrite(motorA1, baseSpeed);
+    analogWrite(motorB1, baseSpeed - heavyTurnAdjustment);
   }
-}
-
-void goStraight() {
-  analogWrite(motorA1, baseSpeed);
-  analogWrite(motorB1, baseSpeed);
-}
-
-void hardRightTurn() {
-  analogWrite(motorA1, baseSpeed - hardTurnAdjustment);
-  analogWrite(motorB1, baseSpeed);
-}
-
-void strongRightTurn() {
-  analogWrite(motorA1, baseSpeed - strongTurnAdjustment);
-  analogWrite(motorB1, baseSpeed);
-}
-
-void veerLeft() {
-  analogWrite(motorA1, baseSpeed);
-  analogWrite(motorB1, baseSpeed - veerAdjustment);
-}
-
-void veerRight() {
-  analogWrite(motorA1, baseSpeed - veerAdjustment);
-  analogWrite(motorB1, baseSpeed);
-}
-
-void strongLeftTurn() {
-  analogWrite(motorA1, baseSpeed);
-  analogWrite(motorB1, baseSpeed - strongTurnAdjustment * 2);
-}
-
-void hardLeftTurn() {
-  analogWrite(motorA1, baseSpeed);
-  analogWrite(motorB1, baseSpeed - hardTurnAdjustment * 2);
-}
-
-void rotateHeavyLeft() {
-  analogWrite(motorA1, baseSpeed - heavyTurnAdjustment);
-  analogWrite(motorB1, baseSpeed);
-}
-
-void rotateHeavyRight() {
-  analogWrite(motorA1, baseSpeed);
-  analogWrite(motorB1, baseSpeed - heavyTurnAdjustment);
-}
-
-void moveForward() {
-  digitalWrite(motorA2, baseSpeed);
-  digitalWrite(motorB2, baseSpeed);
-  delay(1000);
-  if (digitalRead(motorA2) == HIGH && digitalRead(motorB2) == HIGH) {
+  else if(allCenterSensorsActive){
+    digitalWrite(motorA2, baseSpeed);
+    digitalWrite(motorB2, baseSpeed);
+    delay(300);
+    if(allCenterSensorsActive){
     analogWrite(motorA1, 0);
     analogWrite(motorB1, 0);
+    }
   }
   digitalWrite(motorA2, LOW);
   digitalWrite(motorB2, LOW);
+
 }
